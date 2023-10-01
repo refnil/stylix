f2819b0653502abd6cd7d7c0531f6190f59457d8
Martin Lavoie <broemartino@gmail.com>
2023-10-01 18:57:19 -0300

    Workaround fish script

--- modules/fish/prompt.nix
+++ modules/fish/prompt.nix
@@ -10,6 +10,9 @@ let
     };
   };
 in ''
-  source ${theme}
-  base16-${config.lib.stylix.colors.slug}
+  # https://github.com/tomyun/base16-fish/issues/7
+  if status --is-interactive && test -z "$TMUX"
+    source ${theme}
+    base16-${config.lib.stylix.colors.slug}
+  end
 ''
