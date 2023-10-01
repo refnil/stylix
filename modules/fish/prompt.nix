8c9c08778de070389a954b58d8b8e756f17675bd
Martin Lavoie <broemartino@gmail.com>
2023-10-01 18:57:19 -0300

    Workaround fish script

--- modules/fish/prompt.nix
+++ modules/fish/prompt.nix
@@ -5,6 +5,9 @@ let
     templateRepo = config.lib.stylix.templates.base16-fish;
   };
 in ''
-  source ${theme}
-  base16-${config.lib.stylix.colors.slug}
+  # https://github.com/tomyun/base16-fish/issues/7
+  if status --is-interactive && test -z "$TMUX"
+    source ${theme}
+    base16-${config.lib.stylix.colors.slug}
+  end
 ''
